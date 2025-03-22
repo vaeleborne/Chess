d4fb124f46f710e93e859206ce08910dc989b362
Dylan <dmorgan98101+github@gmail.com>
2025-03-22 14:02:01 -0700

    Started stubbing out some classes, implemented a Position struct

---
+++ ChessProgram/Board.hpp
@@ -0,0 +1,24 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+/**
+ * @file    Board.hpp
+ * @author  Dylan Morgan
+ * @brief   Defines the Board for the chess engine.
+ * @date    2025-03-22
+ * @version 1.0
+ * @copyright MIT License
+
+ *
+ * This class is part of the ChessEngine and implements the Board class.
+ * 
+ * TODO: Implement Class!
+ */
+#include "Colorful_IO.hpp"
+
+namespace Chess
+{
+
+}
+
+
+#endif
---
+++ ChessProgram/Piece.hpp
@@ -0,0 +1,34 @@
+#ifndef PIECE_HPP
+#define PIECE_HPP
+/**
+ * @file    Piece.hpp
+ * @author  Dylan Morgan
+ * @brief   Defines the abstract class for Piece 
+ * @date    2025-03-22
+ * @version 1.0
+ * @copyright MIT License
+
+ *
+ * This class is part of the ChessEngine and implements the Piece abstract class.
+ *
+ * TODO: Implement Class!
+ */
+
+#include "Position.hpp"
+
+namespace Chess
+{
+	class Piece
+	{
+	public: 
+		enum class Color {WHITE, BLACK};
+	protected:
+		Color color;
+
+	private:
+
+
+	};
+}
+
+#endif
